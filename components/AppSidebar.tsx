98b98e05f816f7dd2cd9aba74b51d9b5f759c8e3
Harsh <harshchalwadi027@gmail.com>
2025-07-19 00:45:48 +0530

    made a sidebar

---
+++ components/AppSidebar.tsx
@@ -0,0 +1,63 @@
+import { Home, LayoutDashboardIcon, SendIcon, CreditCard } from "lucide-react"
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+
+// Menu items.
+const items = [
+  {
+    title: "Home",
+    url: "/",
+    icon: Home,
+  },
+  {
+    title: "Dashboard",
+    url: "/dashboard",
+    icon: LayoutDashboardIcon,
+  },
+  {
+    title: "Transactions",
+    url: "/transactions",
+    icon: CreditCard,
+  },
+  {
+    title: "P2p Transfer",
+    url: "/p2p",
+    icon: SendIcon,
+  },
+
+]
+
+export function AppSidebar() {
+  return (
+    <Sidebar className="bg-white text-black dark:bg-neutral-900 dark:text-white">
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Services</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  )
+}
