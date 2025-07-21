722c2e8ae3478db9bbf594da3e71a0a7892f77b9
Harsh <harshchalwadi027@gmail.com>
2025-07-21 23:58:06 +0530

    completed the logics

--- components/AddMoney.tsx
+++ components/AddMoney.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { Button } from "@/components/ui/button"
 import {
   Card,
@@ -10,8 +11,24 @@ import {
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { SelectBank } from "./SelectBank"
+import {useState} from 'react'
+import { createOnRampTransaction } from "@/lib/actions/createOnRampTransaction"
+import { toast } from "sonner"
+
+const SUPPORTED_BANKS = [{
+    name: "HDFC Bank",
+    redirectUrl: "https://netbanking.hdfcbank.com"
+}, {
+    name: "Axis Bank",
+    redirectUrl: "https://www.axisbank.com/"
+}];
+
 
 export function AddMoneyCard() {
+  const [redirectUrl, setRedirectUrl] = useState(SUPPORTED_BANKS[0]?.redirectUrl);
+    const [amount, setAmount] = useState(0)
+    const [provider, setProvider] = useState(SUPPORTED_BANKS[0]?.name || "")  
+  
   return (
     <Card className="max-w-sm w-full ">
       <CardHeader>
@@ -30,19 +47,31 @@ export function AddMoneyCard() {
                 type="amount"
                 placeholder="ex: $2000"
                 required
+                onChange={(e)=> setAmount(Number(e.target.value))}
                 />
             </div>
             <div className="flex flex-col gap-6">
             <div className="grid gap-2">
                 <Label htmlFor="password">Bank</Label>
-                <SelectBank/>
+
+                <SelectBank onSelect={(value)=>{
+                  setRedirectUrl(SUPPORTED_BANKS.find(x => x.name === value)?.redirectUrl || " ")
+                  setProvider(SUPPORTED_BANKS.find(x => x.name === value)?.name || " ")
+                }} options={SUPPORTED_BANKS.map(x =>({
+                  key : x.name,
+                  value: x.name
+                }))}/>
+
               </div>
             </div>
           </div>
         </form>
       </CardContent>
       <CardFooter className="flex-col gap-2 ">
-        <Button type="submit" className="w-full cursor-pointer">
+        <Button type="submit" className="w-full cursor-pointer" onClick={async()=>{
+          await createOnRampTransaction(amount*100 , provider)
+          window.location.href = redirectUrl || ""
+        }}>
           Add Money
         </Button>
       </CardFooter>
