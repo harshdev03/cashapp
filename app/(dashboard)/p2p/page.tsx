587fc02bbeb698d916db2e035189093d02a6efeb
Harsh <harshchalwadi027@gmail.com>
2025-07-21 23:57:41 +0530

    made the p2p txn and still more need to be added

--- app/(dashboard)/p2p/page.tsx
+++ app/(dashboard)/p2p/page.tsx
@@ -1,8 +1,11 @@
+import { P2pCard } from '@/components/P2pCard'
 import React from 'react'
 
 const P2p = () => {
   return (
-    <div>P2p</div>
+    <div className='flex w-full justify-center items-center px-15 md:px-44'>
+      <P2pCard/>
+    </div>
   )
 }
 
