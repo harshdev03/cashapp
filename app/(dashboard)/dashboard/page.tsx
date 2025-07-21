308942f969632c6d85a7822d2e074c5e2a64ed64
Harsh <harshchalwadi027@gmail.com>
2025-07-21 23:56:02 +0530

    dashboard will be added sooon

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
 
-const Dashboard = () => {
+
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
+import { HammerIcon } from "lucide-react"
+const Dashboard = ()=> {
   return (
-      <div>
-        Dashboard
-      </div>
+<div className="flex justify-center max-w-md">
+<Alert className="h-30 font-semibold w-100">
+  <HammerIcon className="h-2 w-5"/>
+  <AlertTitle>
+   Work in Progress</AlertTitle>
+  <AlertDescription className="text-[15px]">
+    The Dashboard is under development and will be available soon. <br />
+    You can use all other services over here.!
+  </AlertDescription>
+</Alert>
+</div>   
   )
 }
 
