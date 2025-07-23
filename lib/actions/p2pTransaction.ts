50ee130c01ffdc72e17f1494f887117ac4289a61
Harsh <harshchalwadi027@gmail.com>
2025-07-23 22:19:38 +0530

    changed the update to upsert  update if exists, else insert (create)

--- lib/actions/p2pTransaction.ts
+++ lib/actions/p2pTransaction.ts
@@ -7,50 +7,52 @@ export async function p2pTransfer(to: string, amount: number) {
     const session = await getServerSession(authOptions);
     const from = session?.user?.id;
     if (!from) {
-        return {
-            message: "Error while sending"
-        }
+        return { message: "Error while sending" };
     }
+
     const toUser = await db.user.findUnique({
-        where: {
-            number: to
-        }
+        where: { number: to }
     });
 
     if (!toUser) {
-        return {
-            message: "User not found"
-        }
+        return { message: "User not found" };
     }
+
     await db.$transaction(async (tx) => {
         await tx.$queryRaw`SELECT * FROM "Balance" WHERE "userId" = ${Number(from)} FOR UPDATE`;
+
         const fromBalance = await tx.balance.findUnique({
             where: { userId: Number(from) },
-          });
-          if (!fromBalance || fromBalance.amount < amount) {
-            throw new Error('Insufficient funds');
-          }
+        });
 
-          await tx.balance.update({
+        if (!fromBalance || fromBalance.amount < amount) {
+            throw new Error("Insufficient funds");
+        }
+
+        await tx.balance.update({
             where: { userId: Number(from) },
             data: { amount: { decrement: amount } },
-          });
+        });
 
-          await tx.balance.update({
+        await tx.balance.upsert({
             where: { userId: toUser.id },
-            data: { amount: { increment: amount } },
-          });
+            update: { amount: { increment: amount } },
+              create: {
+                userId: toUser.id,
+                amount: amount,
+                locked: 0, 
+        },
+        });
 
-          await tx.p2pTransfer.create({
-                data:{
-                    fromUserId : Number(from),
-                    toUserId : toUser.id,
-                    amount,
-                    timestamp : new Date()
-                }
-          })
-    
-          
+        await tx.p2pTransfer.create({
+            data: {
+                fromUserId: Number(from),
+                toUserId: toUser.id,
+                amount,
+                timestamp: new Date()
+            }
+        });
     });
-    
+
+    return { message: "Transfer successful" };
 }
