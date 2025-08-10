58a3bc99bb21518f2b56b92547949c237f735f8e
Harsh <harshchalwadi027@gmail.com>
2025-08-10 18:44:40 +0530

    nothing changed then also this fck git

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,6 +1,7 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
 import { AppSidebar } from "@/components/AppSidebar"
 
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider>
